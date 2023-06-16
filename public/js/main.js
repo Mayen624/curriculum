8d2165a21d9f34c5819db9bfae4af3ffa69539c4
Mayen624 <juliomayen300@gmail.com>
2023-06-15 22:15:15 -0300

    Beginning of SV, V2

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,25 @@
-console.log('js works!')
+fetch('./public/json/data.json')
+.then(res => res.json())
+.then((data) => {
+
+    const relevantData = data.relevantInfo;
+    const profileData = data.profile;
+    const trainingData = data.training;
+    const experienceData = data.experience;
+    const idioms = data.idioms;
+    const personalData = data.personalData;
+    const techs = data.technologies;
+
+    //Profile section
+    const profile = document.getElementById('profile-content');
+    profile.textContent = profileData.data;
+
+    //Training section
+
+    const formation = document.getElementById('formation-content');
+    formation.textContent = trainingData
+
+    
+    
+})
+.catch(error => console.log(error));
